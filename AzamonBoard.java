8179bac326f2a96c2cfd5ce7da3d65999e17e194
BuildTools <unconfigured@null.spigotmc.org>
2020-10-15 23:04:30 +0200

    AzamonBoard actualizado

--- AzamonBoard.java
+++ AzamonBoard.java
@@ -1,51 +1,61 @@
 package IA.Azamon;
 
-import java.util.ArrayList;
-
 public class AzamonBoard {
-    private IA.Azamon.AzamonInfo aInfo;
-    private ArrayList<Integer> assignedOfer;
-    private ArrayList<Double> actualOferWeight;
-    
-    
+    private IA.Azamon.AzamonInfo azamonInfo;
+    private int[] assignedOffer;
+    private double[] actualWeight;
+
+    //CONSTRUCTORES
     public AzamonBoard(IA.Azamon.AzamonInfo aInfo) {
-        this.aInfo = aInfo;
-        assignedOfer = new ArrayList<Integer>(aInfo.getPaquetes().size());
-        actualOferWeight = new ArrayList<Double>(aInfo.getTransporte().size());
-        fillOfDoubleZero(actualOferWeight);
-    }
-
-    private void fillOfDoubleZero(ArrayList list) {
-        for(int i=0;i<aInfo.getTransporte().size();++i) {
-            list.add(0.0D);
-        }
-    }
-
-    private boolean isPrioritySatisfable(Oferta oferta, Paquete paquete) {
-        int dias = oferta.getDias();
-        int prioridad = paquete.getPrioridad();
-        if (prioridad==0 && dias==1) return true;
-        if (prioridad==1 && (dias==2 || dias==3)) return true;
-        if (prioridad==2 && (dias>3)) return true;
-        return false;
+        this.azamonInfo = aInfo;
+        assignedOffer = new int[aInfo.getPaquetes().size()];
+        actualWeight = new double[aInfo.getTransporte().size()];
+        for(double value : actualWeight) value=0.0D;
     }
 
     public void generateInicialState() {
-        for (Paquete paquete : aInfo.getPaquetes()) {
-            int oferPoint = 0;
-            for(Oferta oferta : aInfo.getTransporte()) {
+        int packagePointer = 0;
+        for (Paquete paquete : azamonInfo.getPaquetes()) {
+            int offerPointer = 0;
+            for(Oferta oferta : azamonInfo.getTransporte()) {
                 double maxWeight = oferta.getPesomax();
-                double sumWeight = actualOferWeight.get(oferPoint) + paquete.getPeso();
+                double sumWeight = actualWeight[offerPointer] + paquete.getPeso();
                 boolean satisfablePriority = isPrioritySatisfable(oferta, paquete);
                 if (sumWeight <= maxWeight && satisfablePriority) {
-                    actualOferWeight.set(oferPoint, sumWeight);
-                    assignedOfer.add(oferPoint);
+                    actualWeight[offerPointer]=sumWeight;
+                    assignedOffer[packagePointer]=offerPointer;
                     break;
                 }
-                ++oferPoint;
+                ++offerPointer;
             }
+            ++packagePointer;
         }
+    }
+    //CONSULTORAS
+
+    public int getAssignedOffer(int i) { return assignedOffer[i]; }
+    public double getActualWeight(int i) { return actualWeight[i]; }
+
+    //MODIFICADORAS
+
+    public boolean setPackageToOffer(int pack, int offer) {
+        double packageWeight = azamonInfo.getPaquetes().get(pack).getPeso();
+        double offerMaxWeight = azamonInfo.getTransporte().get(offer).getPesomax();
+
+        if (packageWeight+actualWeight[assignedOffer[pack]]>offerMaxWeight) return false;
+
+        actualWeight[assignedOffer[pack]]-=packageWeight;
+        actualWeight[offer]+=packageWeight;
+        assignedOffer[pack]=offer;
+        return true;
     }
 
 
+    //FUNCIONES PRIVADAS
+
+    private boolean isPrioritySatisfable(Oferta oferta, Paquete paquete) {
+        int difference = oferta.getDias() - paquete.getPrioridad()*2;
+        if (difference==0 || difference==1) return true;
+        return false;
+    }
 }
