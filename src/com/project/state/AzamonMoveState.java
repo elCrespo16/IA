5ed5c294e0e52d5f1f238c6cd3dbdd355edd3bf9
Xavi <xavier.caricchio@estudiantat.upc.edu>
2020-10-16 18:22:05 +0200

    subclases state

---
+++ src/com/project/state/AzamonMoveState.java
@@ -0,0 +1,18 @@
+package src.com.project.state;
+
+public class AzamonMoveState implements AzamonState{
+    public final int pack;
+    public final int offer;
+
+    public AzamonMoveState(int pack, int offer) {
+        this.pack=pack;
+        this.offer=offer;
+    }
+
+    @Override
+    public AzamonState updateBoard(AzamonBoard azamonBoard) {
+        AzamonState oldState = new AzamonMoveState(pack,azamonBoard.getAssignedOffer(pack));
+        azamonBoard.__move__(pack,offer);
+        return oldState;
+    }
+}
---
+++ src/com/project/state/AzamonSwapState.java
@@ -0,0 +1,20 @@
+package src.com.project.state;
+
+public class AzamonSwapState implements AzamonState {
+    public final int first;
+    public final int second;
+
+    public AzamonSwapState(int first, int second) {
+        this.first=first;
+        this.second=second;
+    }
+
+    @Override
+    public AzamonState updateBoard(AzamonBoard azamonBoard) {
+        int offerFirst = azamonBoard.getAssignedOffer(first);
+        int offerSecond = azamonBoard.getAssignedOffer(second);
+        azamonBoard.__move__(first,offerSecond);
+        azamonBoard.__move__(second,offerSecond);
+        return this;
+    }
+}
